032ba915da488958be91f121bf4a679acf94c84b
marcelo vinicios <marcelo1.dev@gmail.com>
2022-04-13 13:09:04 -0300

    primeiro commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav><img src="https://flyclipart.com/pokemon-logo-png-images-free-download-pokemon-logo-clipart-179178"/></nav>
+    </div>
+  )
+}
+
+export default NavBar
