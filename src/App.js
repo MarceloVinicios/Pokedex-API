3cf2c7f0c3b5402819411243af8f5cee1504fa46
marcelo vinicios <marcelo1.dev@gmail.com>
2022-06-06 23:18:16 -0300

    sprites adicionadas puxando do api

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React, {useEffect, useState} from 'react'
-import { getPokemons } from './api'
+import React, { useEffect, useState } from 'react'
+import { getPokemonData, getPokemons } from './api'
 import './App.css'
 import NavBar from './components/NavBar'
 import Pokedex from './components/Pokedex'
 import Searchbar from './components/SearchBar'
 
-
-
 function App() {
-
-  const [loading, setLoading] = useState(false);
-  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const [pokemons, setPokemons] = useState([])
   const fetchPokemons = async () => {
     try {
       setLoading(true)
-      const result = await getPokemons();
-      setPokemons(result);
-      setLoading(false);
+      const data = await getPokemons()
+      const promises = data.results.map(async pokemon => {
+        return await getPokemonData(pokemon.url)
+      })
+
+      const results = await Promise.all(promises)
+      setPokemons(results)
+      setLoading(false)
     } catch (error) {
-      console.log("fetchPokemons error: ", error)
+      console.log('fetchPokemons error: ', error)
     }
   }
 
   useEffect(() => {
-    console.log("carregou")
-    fetchPokemons();
+    console.log('carregou')
+    fetchPokemons()
   }, [])
 
   return (
     <div>
       <NavBar />
       <Searchbar />
-      <Pokedex pokemons={pokemons} loading={loading}/>
+      <Pokedex pokemons={pokemons} loading={loading} />
     </div>
   )
 }
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import Pokemon from './Pokemon'
 
 const Pokedex = props => {
   const { pokemons, loading } = props
-  return [
+  return (
     <div>
       <div className="pokedex-header">
-        <h1>pokedex</h1>
+        <h1>Pokedex</h1>
         <div>Paginação:</div>
       </div>
-      {loading ? (<div>Carregando, segura fera...</div>) : (
-      <div className="pokedex-grid"></div>)}
+      {loading ? (
+        <div>Carregando, segura fera...</div>
+      ) : (
+        <div className="pokedex-grid">
+          {pokemons &&
+            pokemons.map((pokemon, index) => {
+              return <Pokemon key={index} pokemon={pokemon} />
+            })}
+        </div>
+      )}
     </div>
-  ]
+  )
 }
 
 export default Pokedex
